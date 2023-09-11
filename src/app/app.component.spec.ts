4a6b0fdc66c9685ce5f1d1d75eb5c9afc6ea9e1d
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-09-11 13:42:06 +0200

    Activate prettier also for .html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
-describe('TestComponent', () => {
+describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
